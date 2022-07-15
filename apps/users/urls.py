031d05292708be7d14aeb3402289854dc4016727
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-07-15 16:47:09 +0300

    hw6

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import AddAvatarView, UserListCreateView, UserRetrieveUpdateDestroyView
+from .views import AddAvatarView, AdminToUserView, UserListCreateView, UserRetrieveUpdateDestroyView, UserToAdminView
 
 urlpatterns = [
     path('', UserListCreateView.as_view()),
     path('/avatars', AddAvatarView.as_view()),
     path('/<int:pk>', UserRetrieveUpdateDestroyView.as_view()),
+    path('/<int:pk>/toadmin', UserToAdminView.as_view()),
+    path('/<int:pk>/touser', AdminToUserView.as_view())
 
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework.generics import ListCreateAPIView, UpdateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from core.permissions import IsSuperUser
-from .serializers import AddAvatarSerializer, UserSerializer, ChangeSuperAdminUser
+
+from .serializers import AddAvatarSerializer, ChangeSuperAdminUser, UserSerializer
 
 UserModel = get_user_model()
 
@@ -27,3 +31,25 @@ class UserRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsSuperUser,)
 
 
+class UserToAdminView(GenericAPIView):  # copied from 5th hw
+    queryset = UserModel
+    serializer_class = UserSerializer
+    permission_classes = (IsSuperUser,)
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if not user.is_staff:
+            user.is_staff = True
+            user.save()
+        serializer = self.serializer_class(user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class AdminToUserView(UserToAdminView):  # copied from 5th hw
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if user.is_staff:
+            user.is_staff = False
+            user.save()
+        serializer = self.serializer_class(user)
+        return Response(serializer.data, status.HTTP_200_OK)
